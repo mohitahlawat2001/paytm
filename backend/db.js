3a861a9b9b3e887406059976c43c43e6cd63b148
Mohit ahlawat <65100859+mohitahlawat2001@users.noreply.github.com>
2024-10-26 13:27:46 +0000

    Refactor user schema and add account schema

--- backend/db.js
+++ backend/db.js
@@ -3,11 +3,28 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://mohitahlawat:mohit12345@cluster0.zmrkc.mongodb.net/paytm?retryWrites=true&w=majority&appName=Cluster0')
 
 const userSchema = new mongoose.Schema({
-    name: String,
+    firstName: String,
+    lastName: String,
     email: String,
     password: String
 });
 
-const User = mongoose.model('User', userSchema);
+const accountSchema = new mongoose.Schema({
+    balance: {
+        type: Number,
+        required: true
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+});
 
-module.exports = User;
+const User = mongoose.model('User', userSchema);
+const Account = mongoose.model('Account', accountSchema);
+
+module.exports = {
+    User,
+    Account
+}
