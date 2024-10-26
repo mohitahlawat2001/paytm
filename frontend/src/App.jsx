860fc20ffdaeddfca737630cde896a2d946c4335
Mohit ahlawat <65100859+mohitahlawat2001@users.noreply.github.com>
2024-10-26 18:00:55 +0000

    Refactor frontend routing and add new pages for SignIn, SignUp, Dashboard, and Send

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
+import { BrowserRouter, Route ,Routes} from 'react-router-dom'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
+import Send from './pages/Send'
 
 function App() {
-
   return (
-    <div>
-        Hello world
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<Send />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
