1f27b1341611004b16859db3f65c433894e05586
Mohit ahlawat <65100859+mohitahlawat2001@users.noreply.github.com>
2024-10-26 13:27:57 +0000

    Refactor backend index.js file and add user routes with MongoDB integration

--- backend/index.js
+++ backend/index.js
@@ -1,43 +1,14 @@
 const express = require("express");
-const User = require("./db");
-
+const bodyParser = require('body-parser')
+const userRouter = require("./routes/index")
+const cors = require('cors')
 const app = express();
 
-app.use(express.json());
+app.use(cors())
+app.use(bodyParser.json());
 
-app.post('/signup',async (req,res)=>{
-    const {name,email,password} = req.body;
-    try{
-        const user = new User({
-            name: name,email: email,password: password
-        })
-        user.save()
-        res.json({
-            msg: 'success'
-        })
-    } catch(error){
-        res.statusCode(400).json({
-            msg: 'error occur'
-        })
-    }
-})
+app.use("/api/v1",userRouter)
 
-app.post('/signin',async(req,res)=>{
-    const {email,password} = req.body;
-    try{
-        const user = await User.findOne({email:email,password:password})
-        if(!user){
-            res.statusCode(404).json({
-                msg: "user not exist"
-            })
-        }
-        
-    }catch (error){
-        res.statusCode(500).json({
-            msg: "internal server error"
-        })
-    }
-})
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
