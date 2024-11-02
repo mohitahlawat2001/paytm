137640996c9786a3c7fc8b0882f741d4fd1de23f
Mohit ahlawat <65100859+mohitahlawat2001@users.noreply.github.com>
2024-11-02 07:07:29 +0000

    Refactor SignIn and SignUp pages for improved layout and user experience

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,8 +1,25 @@
+import BottomWarning from "../components/BottomWarning";
+import Button from "../components/Button";
+import Heading from "../components/Heading";
+import InputBox from "../components/InputBox";
+import SubHeading from "../components/SubHeading";
+
 const SignIn = () => {
     return (
-        <div>
-        <h1>SignIn</h1>
+        <div className="bg-slate-300 min-h-screen flex justify-center">
+      <div className="flex flex-col justify-center">
+        <div className="w-80 bg-white p-8 rounded-lg shadow-lg flex flex-col justify-center text-center">
+          <Heading label={"SignUp"} />
+          <SubHeading label={"enter your credentials"} />
+          <InputBox placeholder={"john@mail.com"} label={"Email"} />
+          <InputBox placeholder={"password"} label={"Password"} />
+          <div className="pt-4">
+            <Button label={"Sign in"} />
+          </div>
+          <BottomWarning label={"Don't have an account?"} buttonText={"Signup"} to={"/"} />
         </div>
+      </div>
+    </div>
     );
     }
 
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,9 +1,28 @@
+import BottomWarning from "../components/BottomWarning";
+import Button from "../components/Button";
+import Heading from "../components/Heading";
+import InputBox from "../components/InputBox";
+import SubHeading from "../components/SubHeading";
+
 const SignUp = () => {
-    return (
-        <div>
-        <h1>SignUp</h1>
+  return (
+    <div className="bg-slate-300 min-h-screen flex justify-center ">
+      <div className="flex flex-col justify-center">
+        <div className="w-80 bg-white p-8 rounded-lg shadow-lg flex flex-col justify-center text-center">
+          <Heading label={"SignUp"} />
+          <SubHeading label={"Enter your information for creating an account"} />
+          <InputBox placeholder={"john"} label={"First Name"} />
+          <InputBox placeholder={"doe"} label={"Last Name"} />
+          <InputBox placeholder={"john@mail.com"} label={"Email"} />
+          <InputBox placeholder={"password"} label={"Password"} />
+          <div className="pt-4">
+            <Button label={"Sign Up"} />
+          </div>
+          <BottomWarning label={"Already have an account?"} buttonText={"Signin"} to={"/signin"} />
         </div>
-    );
-    }
+      </div>
+    </div>
+  );
+};
 
 export default SignUp;
