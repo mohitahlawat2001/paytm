579bd3bac03111b29d95ac500e550091a93fb76a
Mohit ahlawat <65100859+mohitahlawat2001@users.noreply.github.com>
2024-11-02 07:07:45 +0000

    Enhance Dashboard and Send pages with new components and improved layout

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,16 @@
+import Appbar from "../components/Appbar";
+import Balance from "../components/Balance";
+import Users from "../components/Users";
 const Dashboard = () => {
-    return (
-        <div>
-        <h1>Dashboard</h1>
-        </div>
-    );
-    }
+  return (
+    <div>
+      <Appbar />
+      <div className="m-8">
+        <Balance value={1000} />
+        <Users />
+      </div>
+    </div>
+  );
+};
 
 export default Dashboard;
--- frontend/src/pages/Send.jsx
+++ frontend/src/pages/Send.jsx
@@ -1,7 +1,26 @@
 const Send = () => {
     return (
-        <div>
-        <h1>Send</h1>
+        <div className="h-screen flex justify-center items-center bg-slate-300">
+            <div className="bg-white p-8 rounded-lg shadow-lg  flex flex-col justify-center w-96">
+                <h1 className="text-3xl font-bold text-center p-6">Send Money</h1>
+                <div className="px-6 pt-3 pb-1 flex space-x-4 items-center" >
+                    <div className="w-12 h-12 rounded-full bg-green-500 flex items-center justify-center">
+                        <span className="text-white text-2xl">A</span>
+                    </div>
+                    <div className="text-center text-2xl font-semibold mt-2">Friend's Name</div>
+                </div>
+                <div className="text-sm font-medium mx-6 " >
+                    Amount (in Rs)
+                </div>
+                <div className="px-6" >
+                    <input  placeholder="enter amount" className="w-full p-2 border border-gray-300 rounded-lg mt-1" />
+                </div>
+                <div className="px-6 mt-4" >
+                    <button className="w-full bg-green-500 text-white p-2 rounded-lg">Send</button>
+                </div>
+                
+            </div>
+        
         </div>
     );
     }
