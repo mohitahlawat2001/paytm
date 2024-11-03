55afaf6cd7e526d140eb396bb055f02247f3cbef
Mohit ahlawat <65100859+mohitahlawat2001@users.noreply.github.com>
2024-11-03 15:07:41 +0000

    Implement token validation and redirect based on authentication status; enhance routing in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,48 @@
-import { BrowserRouter, Route ,Routes} from 'react-router-dom'
+import { BrowserRouter, Route ,Routes ,Navigate } from 'react-router-dom'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Dashboard from './pages/Dashboard'
 import Send from './pages/Send'
+import {useState, useEffect } from 'react'
+import axios from 'axios'
+const VITE_BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(()=>{
+    checkToken();
+  },[]);
+
+  const checkToken = async ()=>{
+    const token = localStorage.getItem("token");
+    try{
+        const res = await axios.get(`${VITE_BACKEND_URL}/api/v1/user/me`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        setIsLoggedIn(res.data.loggedIn);
+      }catch(error){
+        setIsLoggedIn(false);
+      } finally {
+        setLoading(false);
+      }
+    }
+    
+  if(loading){
+    return <div>Loading...</div>
+  }
+
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<SignUp />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/send" element={<Send />} />
+        <Route path="/" element={isLoggedIn ? <Navigate to="/dashboard" /> : <Navigate to="/signin" />} />
+        <Route path="/signup" element={<SignUp />} />
+          <Route path="/signin" element={<SignIn setIsLoggedIn={setIsLoggedIn}/>} />
+          <Route path="/dashboard" element={isLoggedIn ? <Dashboard /> : <Navigate to="/signin" />} />
+          <Route path="/send" element={isLoggedIn ? <Send /> : <Navigate to="/signin" />} />
         </Routes>
       </BrowserRouter>
     </>
