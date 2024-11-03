06a9c06fa7c70d42c16d26aa260f7c5671b767d3
Mohit ahlawat <65100859+mohitahlawat2001@users.noreply.github.com>
2024-11-03 07:23:00 +0000

    Fetch and display user balance in Dashboard component; update Button styling

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -2,7 +2,7 @@ const Button = ({label,onClick}) =>{
     return(
         <button
             onClick={onClick}
-         className="bg-blue-500 text-white p-2 rounded-md mt-4 w-full ">
+         className="bg-blue-500 text-white p-2 rounded-md w-full ">
             {label}
         </button>
     )
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,12 +1,32 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 import Appbar from "../components/Appbar";
 import Balance from "../components/Balance";
 import Users from "../components/Users";
+
+const VITE_BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 const Dashboard = () => {
+  const [balance, setBalance] = useState(0);
+  useEffect(() => {
+    const fetchBalance = async () => {
+      const token = localStorage.getItem("token");
+      const res = await axios.get(`${VITE_BACKEND_URL}/api/v1/account/balance`, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      setBalance(res.data.balance);
+    };
+    fetchBalance();
+  }, []);
   return (
     <div>
       <Appbar />
       <div className="m-8">
-        <Balance value={1000} />
+        <Balance value={
+          balance
+        } />
         <Users />
       </div>
     </div>
