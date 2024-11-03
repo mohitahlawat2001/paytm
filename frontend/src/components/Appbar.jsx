bb41200e0e5dbc166aac7e6ee6694c5a1adedfd0
Mohit ahlawat <65100859+mohitahlawat2001@users.noreply.github.com>
2024-11-03 07:48:50 +0000

    Fetch and display user first and last name in Appbar; implement user data retrieval on component mount

--- frontend/src/components/Appbar.jsx
+++ frontend/src/components/Appbar.jsx
@@ -1,17 +1,37 @@
+import {useState, useEffect } from "react";
+import axios from "axios";
+const VITE_BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+
 const Appbar = ()=>{
+    const [firstName, setFirstName] = useState("Hello");
+    const [lastName, setLastName] = useState("");
+    useEffect(()=>{
+        getUser();
+    },[]);
+    const getUser = async ()=>{
+        const token = localStorage.getItem("token");
+        const res = await axios.get(`${VITE_BACKEND_URL}/api/v1/user/me`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        console.log(res.data);
+        setFirstName(res.data.user.firstName);
+        setLastName(res.data.user.lastName);
+    }
     return (
         <div className="shadow h-14 flex justify-between">
-            <div className="flex flex-col justify-center h-full ml-4 ">
-                AppName
+            <div className="flex flex-col justify-center h-full ml-4 font-bold ">
+                PayDone
             </div>
             <div className="flex">
                 <div className="flex flex-col justify-center h-full mr-4">
-                    Name 
+                  Welcome  {firstName} {lastName}
                 </div>
                 <div className="h-12 w-12 rounded-full bg-slate-200 flex justify-center my-1 mr-4">
 
                 <div className="flex flex-col justify-center h-full">
-                    p
+                    { "U" && firstName[0].toUpperCase()}
                 </div>
                 </div>
             </div>
